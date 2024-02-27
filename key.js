ff49e4aeabfca036922241f0fa69a6cf94ebea08
Rythmokay <129789738+Rythmokay@users.noreply.github.com>
2024-02-27 20:32:01 +0000

    API

---
+++ key.js
@@ -0,0 +1,2 @@
+//Assign the copied API key to the 'key' variable
+key = "e19fc5a89e57311c33a1720bdc467d51";
--- script.js
+++ script.js
@@ -11,7 +11,7 @@ let getWeather = () => {
   }
   //If input field is NOT empty
   else {
-    let url = `https://api.openweathermap.org/data/2.5/weather?q=${cityValue}&appid=${key}&units=metric`;
+    let url = `https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}`;
     //Clear the input field
     cityRef.value = "";
     fetch(url)
